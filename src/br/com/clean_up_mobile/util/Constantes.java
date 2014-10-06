b810faede23eaa8f12b5cdb36a66204ac4d844ed
George <george.luiz.silva@gmail.com>
2014-10-06 20:06:21 -0300

    trazendo as diaristas do servidor

--- src/br/com/clean_up_mobile/util/Constantes.java
+++ src/br/com/clean_up_mobile/util/Constantes.java
@@ -2,9 +2,11 @@ package br.com.clean_up_mobile.util;
 
 public class Constantes {
 	// WebService
-	public static final String IP_SERVIDOR = "192.168.25.2";
+	public static final String IP_SERVIDOR = "10.1.2.8";
 	public static final String POST_LOGIN = "http://" + IP_SERVIDOR
 			+ ":8080/cleanUp/login/mobile";
 	public static final String POST_CADASTRO = "http://" + IP_SERVIDOR
 			+ ":8080/cleanUp/public/cadastro/mobile/add";
+	public static final String GET_DIARISTAS = "http://" + IP_SERVIDOR
+			+ ":8080/cleanUp/mobile/cliente/listDiaristas";
 }
