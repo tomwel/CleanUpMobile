ed8239b8f0034fb83b891e350659f560dac2e8e7
pedrobittar <pedro_bittar_rmk@hotmail.com>
2014-11-11 19:43:56 -0300

    modificação

--- src/br/com/clean_up_mobile/fragment/DetalheServicoFragment.java
+++ src/br/com/clean_up_mobile/fragment/DetalheServicoFragment.java
@@ -1,5 +1,6 @@
 package br.com.clean_up_mobile.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,16 +10,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import br.com.clean_up_mobile.R;
+import br.com.clean_up_mobile.db.UsuarioDB;
 import br.com.clean_up_mobile.model.Diarista;
+import br.com.clean_up_mobile.model.Perfil;
 import br.com.clean_up_mobile.model.Servico;
 import br.com.clean_up_mobile.model.StatusServico;
+import br.com.clean_up_mobile.model.Usuario;
+import br.com.clean_up_mobile.task.WebService;
 import br.com.clean_up_mobile.util.Constantes;
 import br.com.clean_up_mobile.util.Util;
-import br.com.clean_up_mobile.vo.PessoaVO;
 
 public class DetalheServicoFragment extends Fragment implements OnClickListener{
 	
 	Servico serv = new Servico();
+	Usuario usuarioLogado;
 	TextView nomeCliente;
 	TextView nomeDiarista;
 	TextView data;
@@ -28,6 +33,8 @@ public class DetalheServicoFragment extends Fragment implements OnClickListener{
 	Button buttonCancel;
 	Button buttonOk;
 	StatusServico status;
+	String mensagem;
+	UsuarioDB db;
 	
 	public static DetalheServicoFragment novaInstancia(Diarista diarista) {
 		Bundle args = new Bundle();
@@ -52,10 +59,12 @@ public class DetalheServicoFragment extends Fragment implements OnClickListener{
 		valor = (TextView) view.findViewById(R.id.textViewValor);
 		buttonCancel = (Button) view.findViewById(R.id.buttonCancel);
 		buttonOk = (Button) view.findViewById(R.id.buttonOk);
+		db = new UsuarioDB(getActivity().getApplicationContext());
+		usuarioLogado = db.listaUsuario();
 		
 		serv = (Servico)getArguments().getSerializable("servico");
 		
-		nomeCliente.setText("joao");//serv.getCliente().getNome()
+		nomeCliente.setText(serv.getCliente().getNome());
 		nomeDiarista.setText(serv.getDiarista().getNome());
 		//data.setText();
 		endereco.setText(serv.getEndereco());
@@ -64,15 +73,28 @@ public class DetalheServicoFragment extends Fragment implements OnClickListener{
 		descricao.setText(serv.getDescricao());
 		valor.setText(String.valueOf(serv.getValor()));
 		
-		if(serv.getStatus().equals("PENDENTE")){
-			buttonCancel.setText("Recusar");
-			buttonOk.setText("Aceitar");
-		}else if(serv.getStatus().equals("ATIVO")){
-			buttonCancel.setVisibility(View.GONE);
-			buttonOk.setText("Cancelar");
+		if(usuarioLogado.getPerfil().equals(Perfil.ROLE_DIARIST)){
+			if(serv.getStatus().equals("PENDENTE")){
+				buttonCancel.setText("Recusar");
+				buttonOk.setText("Aceitar");
+			}else if(serv.getStatus().equals("ATIVO")){
+				buttonCancel.setVisibility(View.GONE);
+				buttonOk.setText("Cancelar");
+			}else{
+				buttonCancel.setVisibility(View.GONE);
+				buttonOk.setVisibility(View.GONE);
+			}
 		}else{
-			buttonCancel.setVisibility(View.GONE);
-			buttonOk.setVisibility(View.GONE);
+			if(serv.getStatus().equals("PENDENTE")){
+				buttonCancel.setVisibility(View.GONE);
+				buttonOk.setVisibility(View.GONE);
+			}else if(serv.getStatus().equals("ATIVO")){
+				buttonCancel.setVisibility(View.GONE);
+				buttonOk.setText("Cancelar");
+			}else{
+				buttonCancel.setVisibility(View.GONE);
+				buttonOk.setVisibility(View.GONE);
+			}
 		}
 		
 		return view;
@@ -82,17 +104,65 @@ public class DetalheServicoFragment extends Fragment implements OnClickListener{
 	public void onClick(View v) {
 		switch(v.getId()) {
         case R.id.buttonOk:
-          
+          if(buttonOk.getText().equals("Aceitar")){
+        	  confirmarServico(serv);
+          }else{
+        	  cancelaServico(serv);
+          }
           break;
         case R.id.buttonCancel:
-        	
+        	cancelaServico(serv);
           break;
       }
 	}
 	
 	public void cancelaServico(Servico servico) {
-		//if (Util.existeConexao(getApplicationContext()))
-			//new HttpAsyncTask(Constantes.GET_CANCELASERVICO, servico).execute();
+		// somente 2 dias antes
+		if (Util.existeConexao(getActivity().getApplicationContext()))
+			new HttpAsyncTask(Constantes.POST_CANCELASERVICO, servico).execute();
 	}
 	
+	public void confirmarServico(Servico servico) {
+		//somente se a data do servico não tiver passado
+		if (Util.existeConexao(getActivity().getApplicationContext()))
+			new HttpAsyncTask(Constantes.POST_CONFIRMASERVICO, servico).execute();
+	}
+	
+	private class HttpAsyncTask extends AsyncTask<Void, Void, String> {
+
+		private Object o;
+		private String url;
+
+		public HttpAsyncTask(String url, Object o) {
+			this.o = o;
+			this.url = url;
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		@Override
+		protected String doInBackground(Void... params) {
+			return WebService.getREST(url, o);
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+
+			/*try {
+				if (prgDialog.isShowing()) {
+					prgDialog.dismiss();
+				}
+				if (result != null) {
+
+					JSONObject obj = new JSONObject(result);
+					
+					}
+				} catch (JSONException e) {
+				Util.criarToast(getActivity().getApplicationContext(),
+						R.string.msgDeErroWebservice);
+				}*/
+			}
+		}
 }
