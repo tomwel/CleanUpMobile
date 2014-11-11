ee19cb0ddc56667249ade762d0f01f562257cd67
pedrobittar <pedro_bittar_rmk@hotmail.com>
2014-11-11 20:01:19 -0300

    Adicionando URLs a Classe Constantes

--- src/br/com/clean_up_mobile/util/Constantes.java
+++ src/br/com/clean_up_mobile/util/Constantes.java
@@ -11,4 +11,10 @@ public class Constantes {
 			+ ":8080/cleanUp/mobile/cliente/listDiaristas";
 	public static final String CONTRATAR_SERVICO = "http://" + IP_SERVIDOR
 			+ ":8080/cleanUp/mobile/servico/add";
+	public static final String POST_CONFIRMASERVICO = "http://" + IP_SERVIDOR
+            + ":8080/cleanUp/mobile/servico/confirmar";
+    public static final String POST_CANCELASERVICO = "http://" + IP_SERVIDOR
+			+ ":8080/cleanUp/mobile/servico/cancelar";
+    public static final String POST_CLASSIFICASERVICO = "http://" + IP_SERVIDOR
+			+ ":8080/cleanUp/mobile/servico/classifica";
 }
